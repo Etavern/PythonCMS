345951f5772d28bfd6d1d568eef715fb743567cd
muCephei <joneswe@sheridancollege.ca>
2018-03-02 17:57:43 -0500

    Added Login and Signup

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^reset_password/$', auth_views.password_reset, name='reset_password'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
+from .forms import UserSignUp
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 
@@ -14,3 +16,14 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserSignUp(request)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('post_list')
+        else:
+            form = UserSignUp()
+    return render(request, 'signup.html', {'form': form})
