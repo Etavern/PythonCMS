1b71339722fee220cd1ece466e70b71a8a7e6ee7
Elijah <elijahT1023@gmail.com>
2018-03-02 20:14:57 -0500

    Loging page working

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^reset_password/$', auth_views.password_reset, name='reset_password'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'post/new/$', views.post_new, name='post_new'),
