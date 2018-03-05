bbb4f6faa597d5f0501c712b5d1f749e3eb4c61b
Elijah <elijahT1023@gmail.com>
2018-03-04 19:32:34 -0500

    tweeks

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^reset_password/$', views.reset_password, name='reset_password'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'post/new/$', views.post_new, name='post_new'),
+    url(r'post_delete/(?P<pk>\d+)/$', views.post_delete, name='post_delete'),
     url(r'^post/(?P<pk>\d+)/edit$', views.post_edit, name='post_edit'),
     url(r'^account/$', views.account, name='account'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,39 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Avi
+from .models import Post
+from django.http import HttpResponseRedirect
 from django.utils import timezone
-from .forms import UserSignUp, PostForm, UserAviForm
+from .forms import UserSignUp, PostForm, PasswordResetForm
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.models import Group
 from django.conf import settings
 # Create your views here.
 
 
 def post_list(request):
     posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    print(request.user.groups)
+    print(request.user.groups.filter(name='New').count())
+    if request.user.groups.filter(name='New').count() is 0:
+        notnew = 'yes'
+        print(notnew)
+    else:
+        notnew = 'no'
+    return render(request, 'blog/post_list.html', {'posts': posts, 'notnew': notnew})
 
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    if request.user.groups.filter(name='New').count() is 0:
+        notnew = 'yes'
+        print(notnew)
+    else:
+        notnew = 'no'
+    return render(request, 'blog/post_detail.html', {'post': post, 'notnew': notnew})
 
 
 def post_new(request):
@@ -49,21 +65,34 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    messages.success(request, 'You have successfully deleted the post')
+    return redirect('post_list')
+
+
 def reset_password(request):
     print("SOMEONE SHOULD HAVE USED A F$%kING PASSWORD MANAGER")
     return redirect('post_list')
 
 
 def account(request):
-    avi = get_object_or_404(Avi, user=request.user)
-    if request.method == 'POST':
-        form = UserAviForm(request.POST)
-        if form.is_valid:
-            print(valid)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = PasswordChangeForm(request.user, request.POST)
+            if form.is_valid():
+                user = form.save()
+                update_session_auth_hash(request, user)  # Important!
+                messages.success(request, 'Success!')
+                return redirect('account')
+            else:
+                messages.error(request, 'Error!')
+        else:
+            form = PasswordChangeForm(request.user)
+        return render(request, 'blog/account.html', {'form': form})
     else:
-        form = UserAviForm()
-
-    return render(request, 'blog/account.html', {'form': form, 'avi': avi.avatar.url, 'root': settings.MEDIA_ROOT})
+        return HttpResponseRedirect('/login')
 
 
 def signup(request):
@@ -74,8 +103,11 @@ def signup(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
+            group = Group.objects.get(name='New')
+            user.groups.add(group)
             login(request, user)
+            print("A NEW VICTIM HAS BEEN ENTERED")
             return redirect('post_list')
     else:
         form = UserSignUp()
-    return render(request, 'blog/signup.html', {'form': form, 'avi': Avi})
+    return render(request, 'blog/signup.html', {'form': form})
